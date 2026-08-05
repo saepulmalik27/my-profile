acf3f9e48e535f0772ef22ece5cef1fe334d0b03
malik <malik@inspigo.id>
2026-08-05 16:51:54 +0700

    chore: cleanup legacy root next.js files

--- src/components/layout/footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="flex justify-center items-center">
-      Build by Saepul Malik 2025
-    </footer>
-  );
-}
--- src/lib/config/i18n.config.ts
+++
@@ -1,6 +0,0 @@
-export const i18n = {
-    defaultLocale: 'id',
-    locales: ['id', 'en'],
-} as const
-
-export type Locale = (typeof i18n)['locales'][number]
